56ddd233bae729bcccf8fb0e8098aaba8aee9b9f
nyb1711 <nyb1711@gmail.com>
2024-01-09 13:53:01 +0700

    addStaff

--- src/apis/Api.js
+++ src/apis/Api.js
@@ -18,4 +18,79 @@ const getAllProductTypes = async () => {
   }
 };
 
-export default { getAllProductTypes };
+const getAllStaffs = async () => {
+  try {
+    const response = await client.get("/StaffManagement/getStaffs");
+    if (response.data.success) {
+      return response.data.staffs;
+    } else {
+      console.log("not get staffs");
+    }
+  } catch (error) {
+    console.log("error: ", error.message);
+    return [];
+  }
+};
+const addStaff = async (data) => {
+  const endpoint = "/StaffManagement/add";
+  try {
+    const response = await client.post(endpoint, data);
+    return response.data.docId;
+  } catch (error) {
+    console.log("error: ", error.message);
+  }
+};
+const updateStaff = async (data, id) => {
+  const endpoint = "/StaffManagement/update/" + id;
+  try {
+    await client.put(endpoint, data);
+  } catch (error) {
+    console.error("error: ", error.message);
+  }
+};
+const deleteStaff = async (id) => {
+  try {
+    await client.delete("/StaffManagement/delete/" + id);
+  } catch (error) {
+    console.log("error: ", error.message);
+  }
+};
+// const getStaffsBySearch = async (searchCriteria) => {
+//   try {
+//     const queryParams = new URLSearchParams(searchCriteria).toString();
+//     const response = await client.get(`/StaffManagement/Bills?${queryParams}`);
+
+//     if (response.data.success) {
+//       return response.data.bills;
+//     } else {
+//       console.log("not get bills");
+//     }
+//   } catch (error) {
+//     console.log("error: ", error.message);
+//     return [];
+//   }
+// };
+
+const getStaffById = async (id) => {
+  const endpoint = "/StaffManagement/staffById/" + id;
+  try {
+    const response = await client.get(endpoint);
+    if (response.data.success) {
+      return response.data.staffById;
+    } else {
+      console.log("not get staff by id");
+    }
+  } catch (error) {
+    console.log("error: ", error.message);
+    return {};
+  }
+};
+
+export default {
+  getAllProductTypes,
+  getAllStaffs,
+  getStaffById,
+  updateStaff,
+  deleteStaff,
+  addStaff,
+};
--- src/app/(dashboard)/hrmanagement/page.tsx
+++ src/app/(dashboard)/hrmanagement/page.tsx
@@ -16,120 +16,135 @@ import { Eye, File, Trash2, User, UserPlus } from "react-feather";
 import { useEffect, useState } from "react";
 import { SHARE_FUNCTIONS } from "@/constant/function";
 import { useRouter } from "next/navigation";
+import api from "@/apis/Api";
 
-const staffs = [
-  {
-    staffId: "NV001",
-    name: "Nhân viên A",
-    phoneNumber: "0778889991",
-    position: "Nhân viên",
-    gender: true,
-    email: "toilanhanvien@gmail.com",
-    password: "123456789",
-  },
-  {
-    staffId: "NV002",
-    name: "Nhân viên A",
-    phoneNumber: "0778889991",
-    position: "Nhân viên",
-    gender: true,
-    email: "toilanhanvien@gmail.com",
-    password: "123456789",
-  },
-  {
-    staffId: "NV003",
-    name: "Nhân viên A",
-    phoneNumber: "0778889991",
-    position: "Nhân viên",
-    gender: true,
-    email: "toilanhanvien@gmail.com",
-    password: "123456789",
-  },
-  {
-    staffId: "NV004",
-    name: "Nhân viên A",
-    phoneNumber: "0778889991",
-    position: "Nhân viên",
-    gender: false,
-    email: "toilanhanvien@gmail.com",
-    password: "123456789",
-  },
-];
+// const staffs = [
+//   {
+//     staffId: "NV001",
+//     name: "Nhân viên A",
+//     phoneNumber: "0778889991",
+//     position: "Nhân viên",
+//     gender: true,
+//     email: "toilanhanvien@gmail.com",
+//     password: "123456789",
+//   },
+//   {
+//     staffId: "NV002",
+//     name: "Nhân viên A",
+//     phoneNumber: "0778889991",
+//     position: "Nhân viên",
+//     gender: true,
+//     email: "toilanhanvien@gmail.com",
+//     password: "123456789",
+//   },
+//   {
+//     staffId: "NV003",
+//     name: "Nhân viên A",
+//     phoneNumber: "0778889991",
+//     position: "Nhân viên",
+//     gender: true,
+//     email: "toilanhanvien@gmail.com",
+//     password: "123456789",
+//   },
+//   {
+//     staffId: "NV004",
+//     name: "Nhân viên A",
+//     phoneNumber: "0778889991",
+//     position: "Nhân viên",
+//     gender: false,
+//     email: "toilanhanvien@gmail.com",
+//     password: "123456789",
+//   },
+// ];
 
-export default function HRManagement() {
+const HRManagement = () => {
+  const [staffs, setStaffs] = useState([]);
+
+  const getStaffs = async () => {
+    const temp = await api.getAllStaffs();
+    console.log(temp);
+    setStaffs(temp);
+  };
+
   const router = useRouter();
+
+  useEffect(() => {
+    getStaffs();
+  }, []);
   return (
     <main className="flex max-h-screen flex-col fill-white overflow-y-scroll">
-      <div className="z-10 fill-white max-w-5xl w-full font-mono text-sm ">
-        <div className="flex-col top-0 w-screen">
-          <div className="flex-col">
-            <div className=" flex-row pt-8">
-              <label className=" font-semibold text-2xl text-black p-7 pt-11">
-                Quản lý nhân sự
-              </label>
-            </div>
-            <Tabs
-              aria-label="Tabs with underline"
-              style="underline"
-              theme={THEME.tabTheme}
-            >
-              <Tabs.Item title="Quản lý nhân viên">
-                <div style={{ height: 50 }}>
-                  <div
-                    className=" flex fixed right-40 pl-96 "
-                    style={{ marginBottom: 30 }}
-                  >
-                    <TextInput
-                      style={{
-                        backgroundColor: "white",
-                        borderRadius: 20,
-                        color: "black",
-                      }}
-                      id="email4"
-                      type="Search"
-                      rightIcon={HiSearch}
-                      placeholder="Search"
-                      required
-                    />
-                  </div>
-                  <div className=" flex fixed right-1 pr-1">
-                    <Button
-                      style={{
-                        borderRadius: 20,
-                        backgroundColor: "#0156FF",
-                        paddingLeft: 5,
-                        paddingRight: 5,
-                      }}
-                      onClick={() => router.push('/hrmanagement/addstaff')}
-                    >
-                      <UserPlus style={{ marginRight: 5 }} />
-                      Thêm nhân viên
-                    </Button>
-                  </div>
-                </div>
+      <div className="flex-col fixed top-0 w-screen h-screen overflow-y-scroll">
+        <div className="flex-col">
+          <div className=" flex-row pt-8">
+            <label className=" font-semibold text-2xl text-black p-7 pt-11">
+              Quản lý nhân sự
+            </label>
+          </div>
+          <Tabs
+            aria-label="Tabs with underline"
+            style="underline"
+            theme={THEME.tabTheme}
+          >
+            <Tabs.Item title="Quản lý nhân viên">
+              <div style={{ height: 50 }}>
                 <div
-                  style={{
-                    width: 1200,
-                    alignSelf: "center",
-                    marginLeft: 12,
-                    marginRight: 12,
-                  }}
+                  className=" flex fixed right-40 pl-96 "
+                  style={{ marginBottom: 30 }}
                 >
-                  <Table hoverable theme={THEME.tableTheme}>
-                    <Table.Head>
-                      <Table.HeadCell>STT</Table.HeadCell>
-                      <Table.HeadCell>Mã nhân viên</Table.HeadCell>
-                      <Table.HeadCell>Tên nhân viên</Table.HeadCell>
-                      <Table.HeadCell>Email</Table.HeadCell>
-                      <Table.HeadCell hidden>Mật khẩu</Table.HeadCell>
-                      <Table.HeadCell>Giới tính</Table.HeadCell>
-                      <Table.HeadCell>Số điện thoại</Table.HeadCell>
-                      <Table.HeadCell>Chức vụ</Table.HeadCell>
-                      <Table.HeadCell>
-                        <span className="sr-only">Edit</span>
-                      </Table.HeadCell>
-                    </Table.Head>
-                    {staffs.map((staff, index) => {
+                  <TextInput
+                    style={{
+                      backgroundColor: "white",
+                      borderRadius: 20,
+                      color: "black",
+                    }}
+                    id="email4"
+                    type="Search"
+                    rightIcon={HiSearch}
+                    placeholder="Search"
+                    required
+                  />
+                </div>
+                <div className=" flex fixed right-1 pr-1">
+                  <Button
+                    style={{
+                      borderRadius: 20,
+                      backgroundColor: "#0156FF",
+                      paddingLeft: 5,
+                      paddingRight: 5,
+                    }}
+                    onClick={() => router.push("/hrmanagement/addstaff/add")}
+                  >
+                    <UserPlus style={{ marginRight: 5 }} />
+                    Thêm nhân viên
+                  </Button>
+                </div>
+              </div>
+              <div
+                style={{
+                  width: 1200,
+                  alignSelf: "center",
+                  marginLeft: 12,
+                  marginRight: 12,
+                }}
+              >
+                <Table hoverable theme={THEME.tableTheme}>
+                  <Table.Head>
+                    <Table.HeadCell>STT</Table.HeadCell>
+                    <Table.HeadCell>Mã nhân viên</Table.HeadCell>
+                    <Table.HeadCell>Tên nhân viên</Table.HeadCell>
+                    <Table.HeadCell>Email</Table.HeadCell>
+                    <Table.HeadCell hidden>Mật khẩu</Table.HeadCell>
+                    <Table.HeadCell>Giới tính</Table.HeadCell>
+                    <Table.HeadCell>Số điện thoại</Table.HeadCell>
+                    <Table.HeadCell>Chức vụ</Table.HeadCell>
+                    <Table.HeadCell>
+                      <span className="sr-only">Edit</span>
+                    </Table.HeadCell>
+                  </Table.Head>
+                  {staffs.length == 0 ? (
+                    <></>
+                  ) : (
+                    staffs.map((staff, index) => {
                       return (
                         <Table.Body
                           className="divide-y bg-teal-200"
@@ -137,7 +152,9 @@ export default function HRManagement() {
                         >
                           <Table.Row
                             className="bg-white dark:border-gray-700 dark:bg-gray-100 px-3 py-2"
-                            onClick={() => alert(index)}
+                            onClick={() =>
+                              router.push(`/hrmanagement/addstaff/${staff.Id}`)
+                            }
                           >
                             <Table.Cell className="whitespace-nowrap font-medium text-black dark:text-black w-1 text-center px-3 py-2">
                               {index + 1}
@@ -155,16 +172,12 @@ export default function HRManagement() {
                               {staff.password}
                             </Table.Cell>
                             <Table.Cell className="w-24 px-3 py-2">
-                              {staff.gender !== null
-                                ? staff.gender
-                                  ? "Nam"
-                                  : "Nữ"
-                                : ""}
+                              {staff.gender}
                             </Table.Cell>
                             <Table.Cell className="w-28 text-right px-3 py-2">
                               {staff.phoneNumber}
                             </Table.Cell>
-                            <Table.Cell className="w-28 text-right px-3 py-2">
+                            <Table.Cell className="w-28 px-3 py-2">
                               {staff.position}
                             </Table.Cell>
                             <Table.Cell className="w-24 px-3 py-2 align-center">
@@ -187,15 +200,16 @@ export default function HRManagement() {
                           </Table.Row>
                         </Table.Body>
                       );
-                    })}
-                  </Table>
-                </div>
-              </Tabs.Item>
-              <Tabs.Item title="Quản lý lương"></Tabs.Item>
-            </Tabs>
-          </div>
+                    })
+                  )}
+                </Table>
+              </div>
+            </Tabs.Item>
+            <Tabs.Item title="Quản lý lương"></Tabs.Item>
+          </Tabs>
         </div>
       </div>
     </main>
   );
-}
+};
+export default HRManagement;
