32c309ff5be21db840ff4744a8942bc82570bdaf
wuan.duc <univerbachtuoc@gmail.com>
2024-01-12 22:09:17 +0700

    add some config

--- src/app/(dashboard)/home/page.tsx
+++ src/app/(dashboard)/home/page.tsx
@@ -13,15 +13,19 @@ import SearchInput from "@/components/searchinput";
 import { THEME } from "@/constant/theme";
 import { HiSearch } from "react-icons/hi";
 import { Eye, File, Trash2, User, UserPlus } from "react-feather";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { SHARE_FUNCTIONS } from "@/constant/function";
 import { useRouter } from "next/navigation";
 import PlaygroundPage from "@/components/main";
+import { Analytics } from "@vercel/analytics/react";
 export default function Home() {
   const router = useRouter();
   return (
     <main className=" max-h-screen overflow-y-scroll">
+      <Suspense>
+      </Suspense>
           <PlaygroundPage></PlaygroundPage>
+          <Analytics></Analytics>
     </main>
   );
 }
